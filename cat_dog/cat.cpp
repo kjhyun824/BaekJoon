e3c10ef256c09bd3cc24d37f27f6755544c2f2ed
Junghyun <woori4829@gmail.com>
2021-03-05 11:12:00 +0900

    cat_dog printing

---
+++ cat_dog/cat.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+const std::string cat = "\\    /\\\n )  ( \')\n(  /  )\n \\(__)|";
+
+int main() {
+	std::cout << cat << std::endl;
+	return 0;
+}
---
+++ cat_dog/dog.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+const std::string dog = "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|";
+
+int main() {
+	std::cout << dog << std::endl;
+	return 0;
+}
